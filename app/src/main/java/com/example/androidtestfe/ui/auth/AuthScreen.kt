8f5a709df734e861391ec18a85f670ede8c04436
SirJackSparow <maulanabima785@gmail.com>
2024-03-05 22:28:25 -0800

    AuthScreen

---
+++ app/src/main/java/com/example/androidtestfe/ui/auth/AuthScreen.kt
@@ -0,0 +1,220 @@
+package com.example.androidtestfe.ui.auth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androidtestfe.R
+
+@Composable
+fun AuthScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(MaterialTheme.shapes.medium)
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        AuthTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = stringResource(id = R.string.email),
+            leadingIcon = Icons.Default.Email
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        AuthTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = stringResource(id = R.string.password),
+            leadingIcon = Icons.Default.Lock,
+            isPassword = true,
+            trailingIcon = {
+                IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                    Icon(
+                        imageVector = if (isPasswordVisible) Icons.Rounded.FavoriteBorder else Icons.Default.FavoriteBorder,
+                        contentDescription = null
+                    )
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        AuthButton(
+            text = stringResource(id = R.string.login),
+            onClick = {
+                // Perform login logic here
+            }
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        AuthToggle(
+            isLogin = false,
+            onToggle = {
+
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AuthTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    leadingIcon: ImageVector,
+    isPassword: Boolean = false,
+    trailingIcon: @Composable (() -> Unit)? = null
+) {
+    var isFocused by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = { newValue ->
+            onValueChange(newValue)
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .onFocusChanged {
+                isFocused = it.isFocused
+            },
+        label = { Text(text = label) },
+        leadingIcon = {
+            Icon(imageVector = leadingIcon, contentDescription = null)
+        },
+        trailingIcon = trailingIcon,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email,
+            imeAction = if (isPassword) ImeAction.Done else ImeAction.Next
+        ),
+        visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedLabelColor = MaterialTheme.colorScheme.primary,
+            focusedBorderColor = MaterialTheme.colorScheme.primary,
+            unfocusedBorderColor = if (isFocused) MaterialTheme.colorScheme.primary else Color.Gray
+        ),
+        textStyle = MaterialTheme.typography.bodyMedium,
+    )
+}
+
+@Composable
+fun AuthButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            contentColor = MaterialTheme.colorScheme.primary
+        )
+    ) {
+        Text(
+            text = text,
+            color = Color.White
+        )
+    }
+}
+
+@Composable
+fun AuthToggle(
+    isLogin: Boolean,
+    onToggle: () -> Unit
+) {
+    val text = if (isLogin) {
+        stringResource(id = R.string.create_account)
+    } else {
+        stringResource(id = R.string.already_have_account)
+    }
+
+    val toggleText = if (isLogin) {
+        stringResource(id = R.string.register_instead)
+    } else {
+        stringResource(id = R.string.login_instead)
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+
+        TextButton(onClick = onToggle) {
+            Text(
+                text = toggleText,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.padding(top = 15.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    AuthScreen()
+}
